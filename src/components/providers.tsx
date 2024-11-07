8f51fe9e86dd67773d7c78604a3d9fc6467be111
Kenan Yildirim <kenan.yildirim@pangea.cloud>
2024-11-07 11:44:49 -0500

    fix(deps): update dependency next-themes to v0.4.3

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,8 +1,7 @@
 'use client'
 
 import * as React from 'react'
-import { ThemeProvider as NextThemesProvider } from 'next-themes'
-import { ThemeProviderProps } from 'next-themes/dist/types'
+import { ThemeProvider as NextThemesProvider, type ThemeProviderProps } from 'next-themes'
 
 import { TooltipProvider } from '@/components/ui/tooltip'
 
